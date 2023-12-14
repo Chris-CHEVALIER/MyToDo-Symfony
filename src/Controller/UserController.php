4f7bd3440a8f26ab993ca49398018ed7e1730b46
Chris <chevalier@chris-freelance.com>
2023-12-14 13:12:14 +0100

    heroku upload

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -11,7 +11,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
-use Symfony\Component\Security\Http\Authenticator\AuthenticatorInterface;
 use Symfony\Component\Security\Http\Authenticator\FormLoginAuthenticator;
 
 class UserController extends AbstractController
